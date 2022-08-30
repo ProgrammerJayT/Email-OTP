2421851aaabbadfc4657d4a51f3495ab258976d3
ProgrammerJayT <theanthem@gmail.com>
2022-08-30 21:32:43 +0200

    Created send OTP method

--- App/app/Http/Controllers/MailControl.php
+++ App/app/Http/Controllers/MailControl.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SendOTP;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailControl extends Controller
 {
@@ -10,6 +12,12 @@ class MailControl extends Controller
     public function index()
     {
         return view('welcome');
+    }
+
+    public function send(Request $req){
+        
+        $email = $req->input();
+        // Mail::to('theanthem8@gmail.com')->send(new SendOTP());
     }
 
     public function getOTP()
