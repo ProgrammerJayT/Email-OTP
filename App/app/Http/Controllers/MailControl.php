9102cc7dd7582070cf47219fb8463144c7a33d4c
ProgrammerJayT <theanthem@gmail.com>
2022-08-30 21:00:29 +0200

    Added mail control methods for sending, and processing OTP

--- App/app/Http/Controllers/MailControl.php
+++ App/app/Http/Controllers/MailControl.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class MailControl extends Controller
 {
     //
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function getOTP()
+    {
+        return view('getOTP');
+    }
 }
