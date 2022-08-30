4c60bf81c6e1842a4df528568cbfdeabcc4b525b
ProgrammerJayT <theanthem8@gmail.com>
2022-08-30 22:54:54 +0200

    Added OTP generate method

--- App/app/Http/Controllers/MailControl.php
+++ App/app/Http/Controllers/MailControl.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Mail\SendOTP;
+use Ichtrojan\Otp\Otp;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Mail;
 
 class MailControl extends Controller
 {
+
+    public $expiryTime = 1; // 1 minute
+    public $otpLength = 6;
     //
     public function index()
     {
@@ -18,11 +22,26 @@ class MailControl extends Controller
     public function send(Request $req){
 
         $email = $req->email;
+        $length = $this->otpLength;
+        $expiryTime = $this->expiryTime;
+
+        $genOTP = new Otp();
+        $otp = $genOTP->generate(
+            $email,
+            $length,
+            $expiryTime
+        );
         
-        Mail::to($email)->send(new SendOTP());
+        Mail::to($email)->send(new SendOTP(
+            $otp->token,
+            $email,
+            $expiryTime
+        ));
+
+        print_r($otp);
     }
 
-    public function getOTP()
+    public function validate()
     {
         return view('getOTP');
     }
