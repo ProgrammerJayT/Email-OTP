fba4245276489da4cf19aba365f4156ef66af185
ProgrammerJayT <theanthem8@gmail.com>
2022-09-03 20:03:50 +0200

    Test otp config variable

--- App/app/Http/Controllers/MailControl.php
+++ App/app/Http/Controllers/MailControl.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Mail\SendOTP;
+use Ichtrojan\Otp\Commands\CleanOtps;
+use Ichtrojan\Otp\Models\Otp as ModelsOtp;
 use Ichtrojan\Otp\Otp;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -12,19 +14,24 @@ use Illuminate\Support\Facades\Session;
 class MailControl extends Controller
 {
 
-    public $expiryTime = 1; // 1 minute
-    public $otpLength = 6;
+    public $expiryTime; // 1 minute
+    public $otpLength; // 6 digits
     //
     public function index()
     {
         return view('welcome');
     }
 
-    public function send(Request $req){
+    public function send(Request $req)
+    {        
+        $this->expiryTime = config('otp.period');
+        $this->otpLength = config('otp.length');
 
         $email = $req->email;
         $length = $this->otpLength;
         $expiryTime = $this->expiryTime;
+
+        dd(self::checkUsage($email));
 
         $genOTP = new Otp();
         $otp = $genOTP->generate(
@@ -32,24 +39,30 @@ class MailControl extends Controller
             $length,
             $expiryTime
         );
+
         
-        Mail::to($email)->send(new SendOTP(
-            $otp->token,
-            $email,
-            $expiryTime
-        ));
 
-        if ($otp->status == 1) {
-            return redirect('api/validate?email='.$email);
-
-        } else {
-            return redirect('/')->with('error', 'Error sending OTP');
-        }
+        // Mail::to($email)->send(new SendOTP(
+        //     $otp->token,
+        //     $email,
+        //     $expiryTime
+        // ));
     }
 
     public function validate()
     {
-        
+
         return view('validate');
+    }
+
+    public function checkUsage ($email) {
+
+        $check = ModelsOtp::where('identifier', $email)->first();
+        
+        if ($check->updated_at->isToday()) {
+            return 'Today is the day';
+        } else {
+            return false;
+        }
     }
 }
