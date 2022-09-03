ae82f9e36c33596c9b1b55271e7bf748fb7f02ae
ProgrammerJayT <theanthem8@gmail.com>
2022-09-03 23:07:54 +0200

    Created usage control controller

--- App/app/Http/Controllers/UsageControl.php
+++ App/app/Http/Controllers/UsageControl.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Ichtrojan\Otp\Models\Otp as OtpModel;
+use Ichtrojan\Otp\Otp;
 use Illuminate\Http\Request;
+use Ichtrojan\Otp\Models\Otp as OtpModel;
 
 class UsageControl extends Controller
 {
@@ -11,10 +12,37 @@ class UsageControl extends Controller
     public function check($email)
     {
         $check = OtpModel::where('identifier', $email)->first();
-        if ($check->usage < 3) {
+
+        if (!$check) {
+            self::create($email);
+
             return true;
         } else {
-            return false;
+            if (self::usage($email)) {
+                self::create($email);
+
+                return true;
+            } else {
+                return false;
+            }
         }
+    }
+
+    public function create($email)
+    {
+        $genOTP = new Otp();
+        $genOTP->generate(
+            $email,
+            config('otp.length'),
+            config('otp.period')
+        );
+    }
+
+    public function usage($email)
+    {
+        $maxUsage = config('otp.usage');
+        $usage = OtpModel::where('identifier', $email)->first();
+
+        return $usage->usage <= $maxUsage ? true : false;
     }
 }
