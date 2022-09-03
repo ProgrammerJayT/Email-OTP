0510cb709b20efe6f6a7e92e9bc9b26305a508e1
ProgrammerJayT <theanthem8@gmail.com>
2022-09-03 20:01:35 +0200

    Modified route from get to post

--- App/routes/api.php
+++ App/routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/send', [MailControl::class, 'send']);
+Route::post('/send', [MailControl::class, 'send']);
 
 Route::get('/validate', [MailControl::class, 'validate']);
