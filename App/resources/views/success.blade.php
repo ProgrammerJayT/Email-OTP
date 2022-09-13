6dc297d2ab8232156fc68c32e4fbbcbaf95f81df
ProgrammerJayT <theanthem8@gmail.com>
2022-09-14 01:37:45 +0200

    Added success message

--- App/resources/views/success.blade.php
+++ App/resources/views/success.blade.php
@@ -8,20 +8,19 @@
         <div class="d-xl-flex flex-column align-items-xl-center">
             <div
                 class="d-flex d-sm-flex d-xl-flex flex-column align-items-center align-items-sm-center align-items-xl-center">
-                <img src="assets/img/Grades-pana.png" style="width: 30%;"></div>
+                <img src="assets/img/Grades-pana.png" style="width: 30%;">
+            </div>
             <div class="d-flex d-sm-flex d-xl-flex flex-column align-items-center align-items-sm-center align-items-xl-center"
                 style="width: 100%;">
                 <form class="d-flex d-xl-flex flex-column align-items-center align-items-xl-center"
                     style="width: 40%;min-width: 70%;">
-                    <div class="alert alert-success d-sm-flex flex-row align-items-sm-center alert-dismissible"
-                        role="alert" id="save-sucess"
-                        style="border: 1px solid #0C6D38;width: 100%;background: var(--bs-success);"><button type="button"
-                            class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button><i
+                    <div class="alert alert-success d-sm-flex flex-row align-items-sm-center" role="alert" id="save-sucess"
+                        style="border: 1px solid #0C6D38;width: 100%;background: var(--bs-success);"><i
                             class="icon ion-checkmark-round me-1" style="color: var(--bs-gray-100);"></i><span
-                            style="color: var(--bs-white);">Intimação arquivada com sucesso<br></span></div>
+                            style="color: var(--bs-white);">OTP validated successfully<br></span></div>
                     <div class="d-flex d-xl-flex flex-column align-items-center align-items-xl-center input-group"
-                        style="width: 100%;margin-top: 25px;"><button class="btn btn-primary" type="submit"
-                            style="width: 50%;border-radius: 20px;">OK</button></div>
+                        style="width: 100%;margin-top: 25px;"><a class="btn btn-primary" role="button"
+                            style="width: 50%;border-radius: 20px;" href="/">OK</a></div>
                 </form>
             </div>
         </div>
