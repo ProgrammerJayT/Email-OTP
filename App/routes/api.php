808993a3f4e3d76367e38bea8e90968f840e84fa
ProgrammerJayT <theanthem8@gmail.com>
2022-09-14 01:02:43 +0200

    Created new routes (getEmail, get OTP, verify)

--- App/routes/api.php
+++ App/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\MailControl;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OTPControl;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/send', [MailControl::class, 'send']);
+Route::post('/getEmail', [MailControl::class, 'getEmail'])->name('getEmail');
 
-Route::post('/validate', [MailControl::class, 'validate'])->name('validate');
+Route::get('/getOTP', [OTPControl::class, 'getOTP'])->name('getOTP');
+
+Route::post('/verify', [OTPControl::class, 'verify'])->name('verify');
